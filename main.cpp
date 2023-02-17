cfd79e7009aeb0155c7f8442934a6bff57d35179
Tatiana Kadykova <tanya-kta@bk.ru>
2023-02-17 15:21:38 +0300

    Implement all arrays with functions and names to test

--- main.cpp
+++ main.cpp
@@ -10,6 +10,26 @@
 #include "sorts/heapSort.cpp"
 #include "sorts/shellSorts.cpp"
 #include "utils.cpp"
+#include "generate.cpp"
+
+std::vector<void (*)(std::vector<int>&)> sorts({
+    bubbleSort, bubbleSortIverson1, bubbleSortIverson12,
+    countingSort, heapSort, simpleInsertionSort, binaryInsertionSort,
+    mergeSort, quickSort, radixSort, selectionSort,
+    shellSort, ciuraShellSort
+});
+std::vector<std::string> sort_names({
+    "BubbleSort", "BubbleSortIverson1", "BubbleSortIverson12",
+    "CountingSort", "HeapSort", "SimpleInsertionSort", "BinaryInsertionSort",
+    "MergeSort", "QuickSort", "RadixSort", "SelectionSort",
+    "ShellSort", "CiuraShellSort"
+});
+std::vector<std::vector<int> (*)(int)> generators({
+    smallRangeArray, largeRangeArray, almostSortedArray, decreasingArray
+});
+std::vector<std::string> gen_names({
+    "random_small_range", "random_large_range", "almost_sorted", "decreasing"
+});
 
 
 int main() {
